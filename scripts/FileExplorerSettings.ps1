9b7f555479b5aae42dadf9e990b3bf5d3c732de0
Jan Rise <jr5632@gmail.com>
2018-12-10 19:42:33 +0100

    Disable Windows features

--- scripts/FileExplorerSettings.ps1
+++ scripts/FileExplorerSettings.ps1
@@ -13,3 +13,6 @@ Set-ItemProperty -Path HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\
 #taskbar where window is open for multi-monitor
 Set-ItemProperty -Path HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced -Name MMTaskbarMode -Value 2
 
+#--- Disable useless features
+Disable-BingSearch
+Disable-GameBarTips
