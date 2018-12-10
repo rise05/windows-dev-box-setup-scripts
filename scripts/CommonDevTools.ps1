cb9e0b58419950206f8cc7256ddf3edaa5a36192
Jan Rise <jr5632@gmail.com>
2018-12-10 22:08:40 +0100

    Attempt to make git install work

--- scripts/CommonDevTools.ps1
+++ scripts/CommonDevTools.ps1
@@ -1,7 +1,7 @@
 
 # tools we expect devs across many scenarios will want
 choco install -y vscode
-choco install -y git --params '"/GitAndUnixToolsOnPath /WindowsTerminal /NoShellIntegration"'
+choco install -y git.install --params "/GitAndUnixToolsOnPath /WindowsTerminal /NoShellIntegration"
 choco install -y 7zip.install
 choco install -y sharex
 choco install -y winscp
